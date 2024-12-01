5df4361475b5a12e25f2e0ea889326744be9b8fb
tvrcars <tvrcars@hotmail.com>
2024-12-01 21:50:26 +0000

    Update Gunicorn config for better resource management

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,7 +1,12 @@
 # Gunicorn configuration file
-timeout = 120  # Increase timeout to 120 seconds
-workers = 2    # Number of worker processes
-threads = 4    # Number of threads per worker
-worker_class = 'gthread'  # Use threads
+timeout = 300  # Increase timeout to 5 minutes
+workers = 1    # Reduce to 1 worker to conserve memory
+threads = 2    # Reduce threads
+worker_class = 'gthread'
 max_requests = 1000
-max_requests_jitter = 50 
+max_requests_jitter = 50
+worker_tmp_dir = '/dev/shm'  # Use RAM for temporary files
+worker_max_requests = 50     # Restart workers periodically
+worker_max_requests_jitter = 10
+limit_request_line = 0       # Unlimited request line size
+limit_request_fields = 32768
