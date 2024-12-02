4aaaed340f04baedff84639430b9e7c92418ef8b
tvrcars <tvrcars@hotmail.com>
2024-12-02 11:43:17 +0000

    Update app.py for Azure deployment configuration

--- app.py
+++ app.py
@@ -128,5 +128,5 @@ def convert():
         return str(e), 500
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='127.0.0.1', port=5000)
+    app.debug = False
+    app.run(host='0.0.0.0', port=8000)
